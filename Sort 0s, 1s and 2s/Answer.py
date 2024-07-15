8a29da38d1bb21ee66a8630a1723aa480ac46e6b
SuryaTech <surya.sangam@techaffy.com>
2024-07-16 02:43:28 +0530

    Add Sort 0 1 2

---
+++ Sort 0s, 1s and 2s/Answer.py
@@ -0,0 +1,21 @@
+#User function Template for python3
+
+class Solution:
+    def sort012(self,arr,n):
+        low = 0
+        mid = len(arr)//2
+        high = len(arr) - 1
+        
+        while mid <= high:
+            if arr[mid] == 0:
+                arr[low], arr[mid] = arr[mid], arr[low]
+                low += 1
+                mid += 1
+            elif arr[mid] == 1:
+                mid += 1
+            else:
+                arr[high], arr[mid] = arr[mid], arr[high]
+                high -= 1
+                
+    
+        return arr
