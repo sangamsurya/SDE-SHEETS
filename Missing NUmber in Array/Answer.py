9a3df6a680bff986b24c088db4c9cec459494717
SuryaTech <surya.sangam@techaffy.com>
2024-07-16 02:47:19 +0530

    Add Missing Number in Array

---
+++ Missing NUmber in Array/Answer.py
@@ -0,0 +1,9 @@
+def missingNumber(self, n, arr):
+        sum1 = (n * (n+1)) // 2
+        
+        sum
+        
+        sum3 = 0
+        for i in arr:
+            sum3 += i
+        return sum1 -sum3
